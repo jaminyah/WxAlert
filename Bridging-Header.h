d29d422a3e25a14bad5fd3bb2370da0e0afed3ef
jaminyah <jaminyah@gmail.com>
2018-03-20 14:20:36 -0400

    Initial tab bar UI and controllers.

---
+++ Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  WxAlert
+//
+//  Created by macbook on 3/20/18.
+//  Copyright © 2018 Jaminya. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+#endif /* Bridging_Header_h */
+#import <sqlite3.h>
---
+++ WxAlert/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  WxAlert
+//
+//  Created by macbook on 3/20/18.
+//  Copyright © 2018 Jaminya. All rights reserved.
+//
+
+import Foundation
