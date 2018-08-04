aef1e3f76da0031218cb99747c420a3c2fa586d1
jaminyah <jaminyah@gmail.com>
2018-08-04 14:07:02 -0400

    Changed app settings table to have 3 sections.

--- WxAlert/AppSettingController.swift
+++ WxAlert/AppSettingController.swift
@@ -11,11 +11,14 @@ import UIKit
 class AppSettingController: UITableViewController {
     
     var appSettings = ["City List", "City Edit"]
+    
+    var delegate: CityProtocol?
+    let rootController = RootController.sharedInstance
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.delegate = rootController
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,18 +29,43 @@ class AppSettingController: UITableViewController {
     // MARK: - Table view data source
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return ONE_SECTION
+        return 3
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.appSettings.count
+        //return self.appSettings.count
+        var count = 0
+        switch (section){
+        case 0:
+            count = 1
+        case 1:
+            count = 1
+        case 2:
+            guard let rowCount = self.delegate?.getCityCount() else {
+                return 0
+            }
+            return rowCount
+        default:
+            count = 0
+        }
+        return count
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "appSettingsCell", for: indexPath)
         
-        cell.textLabel?.text = appSettings[indexPath.row]
+        switch (indexPath.section){
+        case 0:
+            cell.textLabel?.text = appSettings[0]
+        case 1:
+            cell.textLabel?.text = appSettings[1]
+        case 2:
+            cell.textLabel?.text = "weather selection"
+        default:
+            cell.textLabel?.text = "default"
+        }
+        //cell.textLabel?.text = appSettings[indexPath.row]
         return cell
     }
     
@@ -55,6 +83,22 @@ class AppSettingController: UITableViewController {
         default:
             print("Show Add City View")
         }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var title: String?
+        
+        switch section {
+        case 0:
+            title = "Notifications"
+        case 1:
+            title = "Edit City List"
+        case 2:
+            title = "Weather Selection"
+        default:
+            title = nil
+        }
+        return title
     }
     
     /*
